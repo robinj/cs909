432810c74915e6cbb45d4d14ad5161ac3b719edb
Janssens <robin.janssens@gmail.com>
2013-05-08 20:50:55 +0100

    added multithreading coordinator

---
+++ Coordinator.java
@@ -0,0 +1,43 @@
+class Coordinator {
+
+	private int cores;
+	private int workers; 
+	
+	private int notifications; 
+	
+	Coordinator() {
+		this.workers = 0;
+		this.notifications = 0; 
+	
+		this.cores = Runtime.getRuntime().availableProcessors();
+	}
+	
+	public int getCores() {
+		return this.cores; 
+	}
+	
+	public synchronized void dispatchWorker() {
+		this.workers++;
+	}
+	
+	public synchronized void endWorker() {
+		this.workers--;
+	}
+	
+	public synchronized boolean isBusy() {
+		return (this.cores == workers);
+	}
+	
+	public synchronized boolean isNotifying() {
+		return !(this.notifications == 0);
+	}
+	
+	public synchronized void sendNotification() {
+		this.notifications++; 
+		notifyAll(); 
+	}
+	
+	public synchronized void clearNotification() {
+		this.notifications--; 
+	}
+}
